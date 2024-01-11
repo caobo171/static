3d811514f1abddc67dce9374b5a2c18748199282
Cao nguyen <caobo1712000@gmail.com>
2024-01-11 09:59:32 +0700

    Update affine

---
+++ assets/examples/basic/doc-1-kZoxD8.js
@@ -0,0 +1,2 @@
+import{f as t}from"../../static-aza_T-_E.js";import{I as e,J as r}from"../../affine-hYHTZtc7.js";import"../../if-defined-pU-nnT_m.js";const a=await e().init(),o=new r;o.page=a;document.body.appendChild(o);const p=a.getBlockByFlavour("affine:paragraph"),i=p[0];a.updateBlock(i,{text:new t("Hello World!")});
+//# sourceMappingURL=doc-1-kZoxD8.js.map
---
+++ assets/examples/basic/edgeless-kS6TAOp5.js
@@ -0,0 +1,2 @@
+import"../../static-aza_T-_E.js";import{I as e,K as o}from"../../affine-hYHTZtc7.js";import"../../if-defined-pU-nnT_m.js";const a=await e().init(),t=new o;t.page=a;document.body.appendChild(t);
+//# sourceMappingURL=edgeless-kS6TAOp5.js.map
