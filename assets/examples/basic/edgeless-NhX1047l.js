db5e05afeb1027f521944c53a88ab833cea570cd
Cao nguyen <caobo1712000@gmail.com>
2024-01-11 07:10:12 +0700

    init

---
+++ assets/examples/basic/edgeless-NhX1047l.js
@@ -0,0 +1,2 @@
+import"../../static-aza_T-_E.js";import{I as e,K as o}from"../../affine-QJbYoICT.js";import"../../if-defined-BQVeZiir.js";const a=await e().init(),t=new o;t.page=a;document.body.appendChild(t);
+//# sourceMappingURL=edgeless-NhX1047l.js.map
